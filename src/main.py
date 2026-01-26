3eddcdccfa699937d4452464f593283670d581e8
MatsuoReo <reo-robo@outlook.jp>
2026-01-26 11:30:53 +0900

    位置の枠を変更

--- src/main.py
+++ src/main.py
@@ -58,8 +58,8 @@ def main():
 
     pos_xy = np.array([-1.8, 0.5], dtype=float)
     vel_xy = np.array([0.0, 0.0], dtype=float)
-    POS_RANGE_X = 17.5
-    POS_RANGE_Y = 10.0
+    POS_RANGE_X = 50.0
+    POS_RANGE_Y = 35.0
     prev_time = time.perf_counter()
     ACCEL_DEADZONE = 0.09
 
